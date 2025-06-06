f38d344c7d263b7dd4099507ea3ad76457c45ee7
Shisir Basel <110526802+shisirbasel@users.noreply.github.com>
2025-06-06 21:16:07 +0545

    navbar bug fix

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -17,35 +17,35 @@ const Navbar = () => {
           <ColorModeButton />
         </Box>
  
+         <Box display={{ base: "flex", md: "none" }} ml="auto" alignItems="center" gap="4">
+          <ColorModeButton />
           <Drawer.Root size="sm">
-          <Drawer.Trigger asChild>
-            <Button display={{ base: "block", md: "none" }} ml="auto" variant="outline" size="sm">
-              Menu
-            </Button>
-          </Drawer.Trigger>
-          <Portal>
-            <Drawer.Backdrop />
-            <Drawer.Positioner>
-              <Drawer.Content>
-                <Drawer.Header display="flex" justifyContent="space-between" alignItems="center">
-                  <Drawer.Title fontSize="2xl">Menu</Drawer.Title>
-                  <Box display="flex" justifyContent='center' alignItems='center' gap="4">
-                    <ColorModeButton />
+            <Drawer.Trigger asChild>
+              <Button variant="outline" size="sm">
+                Menu
+              </Button>
+            </Drawer.Trigger>
+            <Portal>
+              <Drawer.Backdrop />
+              <Drawer.Positioner>
+                <Drawer.Content>
+                  <Drawer.Header display="flex" justifyContent="space-between" alignItems="center">
+                    <Drawer.Title fontSize="2xl">Menu</Drawer.Title>
                     <Drawer.CloseTrigger asChild>
                       <CloseButton size="xl" />
                     </Drawer.CloseTrigger>
-                  </Box>
-                </Drawer.Header>
-                <Drawer.Body display="flex" flexDirection="column" alignItems="center" justifyContent="center" gap="6" flex="1">
-                  <a href="/#"><Text fontWeight="bold" fontSize="xl">About</Text></a>
-                  <a href="/#education"><Text fontWeight="bold" fontSize="xl">Education</Text></a>
-                  <a href="/#experience"><Text fontWeight="bold" fontSize="xl">Experience</Text></a>
-                  <a href="/#projects"><Text fontWeight="bold" fontSize="xl">Projects</Text></a>
-                </Drawer.Body>
-              </Drawer.Content>
-            </Drawer.Positioner>
-          </Portal>
-        </Drawer.Root>
+                  </Drawer.Header>
+                  <Drawer.Body display="flex" flexDirection="column" alignItems="center" justifyContent="center" gap="6" flex="1">
+                    <a href="/#"><Text fontWeight="bold" fontSize="xl">About</Text></a>
+                    <a href="/#education"><Text fontWeight="bold" fontSize="xl">Education</Text></a>
+                    <a href="/#experience"><Text fontWeight="bold" fontSize="xl">Experience</Text></a>
+                    <a href="/#projects"><Text fontWeight="bold" fontSize="xl">Projects</Text></a>
+                  </Drawer.Body>
+                </Drawer.Content>
+              </Drawer.Positioner>
+            </Portal>
+          </Drawer.Root>
+        </Box>
       </Box>
     </>
   );
